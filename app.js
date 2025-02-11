e65584ae0d26600922708ecb161fcb4f36f799c0
menth0s <marslite@pm.me>
2025-02-11 14:42:24 +0100

    Creating project structure and installing mongoose

--- app.js
+++ app.js
@@ -1,7 +1,22 @@
 const express = require("express");
+const cors = require("cors");
+
 const app = express();
+app.use(cors({ origin: "*" }));
+
 const { PORT = 3001 } = process.env;
 
 app.listen(PORT, () => {
   console.log(`Congrats it's up and running! Port ${PORT} `);
 });
+
+app.get("/", (req, res) => {
+  res.setHeader("Content-Type", "text/html");
+  res.send(`<html>
+    <body>
+    <p>test</p>
+    </body>
+    </html>`);
+});
+
+app.get("/favicon.ico", (req, res) => res.status(204).end());
