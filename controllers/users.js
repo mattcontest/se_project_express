9de43b600659587905c0901367cccf78e5ca7158
menth0s <marslite@pm.me>
2025-02-12 01:11:41 +0100

    Implemented getUsers

--- controllers/users.js
+++ controllers/users.js
@@ -3,7 +3,13 @@ const User = require("../models/user");
 //GET /users
 
 const getUsers = (req, res) => {
-  console.log("Inside the controller");
+  User.find({})
+    .then((users) => {
+      res.send(users);
+    })
+    .catch((err) => {
+      console.error(err);
+    });
 };
 
 module.exports = { getUsers };
