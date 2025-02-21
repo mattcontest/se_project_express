56fec0ec24dcbb65039e9c36d53ce6aaeae23165
menth0s <marslite@pm.me>
2025-02-21 04:39:14 +0100

    Added route for create user

--- routes/index.js
+++ routes/index.js
@@ -3,11 +3,13 @@ const { notFound } = require("../utils/errors");
 
 const userRouter = require("./users");
 const itemRouter = require("./clothingItems");
-const { login } = require("../controllers/users");
+const { login, createUser } = require("../controllers/users");
 
 router.use("/users", userRouter);
 router.use("/items", itemRouter);
 router.post("/signin", login);
+router.post("/signup", createUser);
+
 router.use((req, res) => {
   res.status(notFound).send({ message: "Resource Not Found 404." });
 });
