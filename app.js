ec60377256ef369d296e9b758d7e3e51c9e47787
menth0s <marslite@pm.me>
2025-03-15 02:31:35 +0100

    Implemented NotFoundError and uptaded ESlint with new eslint rule

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const indexRouter = require("./routes/index");
 const errorHandler = require("./middlewares/error-handler");
+
 const app = express();
 
 const { PORT = 3001 } = process.env;
